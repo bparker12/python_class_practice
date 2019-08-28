158d093ad18abe6336729889d7febd9892dcd7fc
bparker12 <49100894+bparker12@users.noreply.github.com>
2019-08-28 10:34:36 -0500

    pizza exercise complete

---
+++ classes.py
@@ -0,0 +1,27 @@
+class Pizza:
+
+    def __init__(self):
+        self.size = 0
+        self.crust = ""
+        self.toppings = []
+
+    def add_topping(self, *args):
+        self.toppings += args
+        # for topping in args:
+        #     self.toppings += [topping]
+
+    def pizza_made(self):
+        space = " and "
+        print(f'I would like a {self.size}-inch, {self.crust} pizza with {space.join(self.toppings)}.')
+
+thick_crust_pizza = Pizza()
+
+thick_crust_pizza.size = 16
+thick_crust_pizza.crust = "thick"
+thick_crust_pizza.add_topping("pineapple")
+thick_crust_pizza.add_topping("canadian bacon")
+
+# print(thick_crust_pizza.add_topping("bacon"))
+
+
+thick_crust_pizza.pizza_made()
