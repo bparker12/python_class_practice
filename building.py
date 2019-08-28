5e3304988232eda2639137cb53d71d138b40d812
bparker12 <49100894+bparker12@users.noreply.github.com>
2019-08-28 11:32:42 -0500

    update to add more instances

--- building.py
+++ building.py
@@ -22,3 +22,25 @@ eight_hundred_eighth = Building("800 8th Street", 12)
 eight_hundred_eighth.purchase("Richard LeFrak")
 eight_hundred_eighth.construct()
 eight_hundred_eighth.building_info()
+
+three_hundred_one_plus_park = Building("301 Plus Park", 3)
+three_hundred_one_plus_park.purchase("Jeff Besos")
+three_hundred_one_plus_park.construct()
+three_hundred_one_plus_park.building_info()
+
+batman_building = Building("AT&T Building Downtown Nashville", 219)
+batman_building.purchase("Mark Cuban")
+batman_building.construct()
+batman_building.building_info()
+
+ninety_six_10_mercedes_drive = Building("9610 Mercedes Drive", 2)
+ninety_six_10_mercedes_drive.purchase("Parks")
+ninety_six_10_mercedes_drive.construct()
+ninety_six_10_mercedes_drive.building_info()
+
+sears_tower = Building("Sears Tower", 10000)
+sears_tower.purchase("NSS")
+sears_tower.construct()
+sears_tower.building_info()
+
+
